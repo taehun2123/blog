53d66d01cd897a7abe85c85caa4cbb4b608f7217
taehun2123 <pyoneng_@naver.com>
2024-04-26 02:41:17 +0900

    Update git ignore

--- src/components/Intro.js
+++
@@ -1,9 +0,0 @@
-export function Intro(){
-  return(
-    <section style={{margin: '10em 0'}}>
-      <div className="whoAmI" data-aos="fade-up" data-aos-anchor-placement="top-center" aos-easing="ease-in-sine" aos-duration="5000">
-        <h1>DEVH을 소개합니다.</h1>
-      </div>
-    </section>
-  )
-}
