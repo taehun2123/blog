5a9c40e25c2a0a659d91f2288a749ded444a5982
taehun2123 <pyoneng_@naver.com>
2024-04-26 02:19:36 +0900

    Update: firebase Key Hided

--- src/firebase.js
+++ src/firebase.js
@@ -7,20 +7,13 @@ import { getFirestore } from "firebase/firestore"
 // Your web app's Firebase configuration
 // For Firebase JS SDK v7.20.0 and later, measurementId is optional
 const firebaseConfig = {
-  // apiKey: process.env.REACT_APP_apiKey,
-  // authDomain: process.env.REACT_APP_authDomain,
-  // projectId: process.env.REACT_APP_projectId,
-  // storageBucket: process.env.REACT_APP_storageBucket,
-  // messagingSenderId: process.env.REACT_APP_messagingSenderId,
-  // appId: process.env.REACT_APP_appId,
-  // measurementId: process.env.measurementId
-  apiKey: "AIzaSyAOfNZONv0BMnaI752R2BDqfyTpiRo_LGI",
-  authDomain: "myblog-a5efe.firebaseapp.com",
-  projectId: "myblog-a5efe",
-  storageBucket: "myblog-a5efe.appspot.com",
-  messagingSenderId: "709900039439",
-  appId: "1:709900039439:web:9a3a04dcb99f0a770ffa7a",
-  measurementId: "G-12NCXG6VMB"
+  apiKey: process.env.REACT_APP_apiKey,
+  authDomain: process.env.REACT_APP_authDomain,
+  projectId: process.env.REACT_APP_projectId,
+  storageBucket: process.env.REACT_APP_storageBucket,
+  messagingSenderId: process.env.REACT_APP_messagingSenderId,
+  appId: process.env.REACT_APP_appId,
+  measurementId: process.env.measurementId,
 };
 
 
